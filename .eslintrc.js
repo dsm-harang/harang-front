c379cae563bfa37cf1e1a279b837cbb5dd3d19f4
ohjunsang <pandati@dsm.hs.kr>
2020-09-09 16:03:11 +0900

    [ADD] prettier, eslint 적용

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-indent': 0,
+  },
+};
