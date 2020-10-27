b203642a4c934baec687cc487ad3e55e47062067
ohjunsang <pandati@dsm.hs.kr>
2020-10-27 19:06:03 +0900

    [ADD] @/assets 사진 추가

--- src/assets/Mypage/index.js
+++ src/assets/Mypage/index.js
@@ -3,3 +3,6 @@ export { default as fullStar } from './fullStar.svg';
 export { default as halfStar } from './halfStar.svg';
 export { default as setting } from './setting.svg';
 export { default as calenderIcon } from './calenderIcon.svg';
+export { default as report } from './report.svg';
+export { default as fiveEmptyStar } from './fiveEmptyStar.svg';
+export { default as fiveFullStar } from './fiveFullStar.svg';
