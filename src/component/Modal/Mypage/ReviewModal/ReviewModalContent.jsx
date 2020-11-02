84214085693adc21a1de8a28638bdfa8ed207ebd
ohjunsang <pandati@dsm.hs.kr>
2020-11-02 22:37:38 +0900

    [CREATE] add server request feat.docs

--- src/component/Modal/Mypage/ReviewModal/ReviewModalContent.jsx
+++ src/component/Modal/Mypage/ReviewModal/ReviewModalContent.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react';
+import { setComment } from '../../../../lib/api/Mypage';
 import Star from '../../../Default/Star';
 import * as S from '../../style';
 import ReviewModalReviewContent from './ReviewModalReviewContent';
@@ -32,8 +33,31 @@ const RequestApproveModalContent = ({
     },
     [id, reviewChange],
   );
+  const getScoreRequestBody = useCallback(
+    (targetId, userId, comment, star) => ({
+      member_id: targetId,
+      user_id: userId,
+      score: star,
+      score_content: comment,
+    }),
+    [],
+  );
+  const setCommentAndGetStatus = useCallback(
+    async (targetId, comment, star) => {
+      const request = getScoreRequestBody(targetId, id, comment, star);
+      try {
+        const { status } = await setComment(id, request);
+        return status;
+      } catch (error) {
+        return error;
+      }
+    },
+    [id],
+  );
   const submitReview = useCallback(() => {
+    deleteData(id);
     deleteModal();
+    setCommentAndGetStatus(id, review, star);
   }, []);
   return (
     <>
