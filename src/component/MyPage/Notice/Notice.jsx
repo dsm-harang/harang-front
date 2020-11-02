e2c2bfcaf10fbdddf3b2fd15e9770856df483c23
ohjunsang <pandati@dsm.hs.kr>
2020-11-02 22:39:08 +0900

    [CREATE] when modal close delete alarm comtent

--- src/component/MyPage/Notice/Notice.jsx
+++ src/component/MyPage/Notice/Notice.jsx
@@ -2,20 +2,35 @@ import React, { useCallback } from 'react';
 import NoticeContent from './NoticeContent';
 import * as S from '../style';
 
-const Notice = ({ noticeContentArray, requestModalOn, reviewModalOn }) => {
+const Notice = ({
+  noticeContentArray,
+  requestModalOn,
+  reviewModalOn,
+  modalIdChange,
+}) => {
   const createNoticeContentArray = useCallback(noticeContentArray => {
     return noticeContentArray.map(({ text, id, type }) => (
       <NoticeContent
         text={text}
         id={id}
-        onClick={() => buttonClickHandler(type, requestModalOn, reviewModalOn)}
+        onClick={() =>
+          buttonClickHandler(id, type, requestModalOn, reviewModalOn)
+        }
         key={text + id}
       />
     ));
   }, []);
   const buttonClickHandler = useCallback(
-    (type, requestModalOn, reviewModalOn) => {
+    (id, type, requestModalOn, reviewModalOn) => {
       type === 'review' ? reviewModalOn() : requestModalOn();
+      if (type === 'review') {
+        modalIdChange(id);
+        reviewModalOn();
+      } else if (type === 'request') {
+        modalIdChange(id);
+        requestModalOn();
+      } else {
+      }
     },
     [],
   );
