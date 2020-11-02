aae15e2273f51300ad0fcfce18df8e4e8916ac3d
ohjunsang <pandati@dsm.hs.kr>
2020-11-02 22:39:45 +0900

    [CREATE] server request feat.docs

--- src/component/MyPage/Mypage.jsx
+++ src/component/MyPage/Mypage.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Profile from './Profile';
 import Notice from './Notice';
 import Storage from './Storage';
@@ -6,25 +7,24 @@ import Modal from '../Modal';
 import Comment from './Comment';
 import AnotherComment from './AnotherCommnet';
 import * as S from './style';
-import { useEffect } from 'react';
+import {
+  getAlarm,
+  getComment,
+  getRecode,
+  getUserInfo,
+  setComment,
+} from '../../lib/api/Mypage';
 
 const MODAL_TYPE_REVIEW = 'review';
 const MODAL_TYPE_REQUEST = 'request';
 const DEFAULT_USER_IMG =
   'https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png';
 const Mypage = () => {
+  const location = useLocation();
+  const [id, idChange] = useState(location.pathname.split('/')[2]);
   const [isMine, isMineChange] = useState(true);
   const [noticeArray, noticeArrayChange] = useState([
-    {
-      text: '어떤어떤 체험의 리뷰를 작성해 주세요.',
-      id: 1,
-      type: MODAL_TYPE_REVIEW,
-    },
-    {
-      text: '무엇무엇동아리에 신청 왔습니다.',
-      id: 1,
-      type: MODAL_TYPE_REQUEST,
-    },
+    { text: '리뷰를 작성해 주세요.', id: 1, type: MODAL_TYPE_REVIEW },
   ]);
   const [storageArray, storageArrayChange] = useState([
     { text: '무슨무슨무슨무슨무슨무슨무슨무슨무슨무슨 체험 참여.', id: 1 },
@@ -41,11 +41,19 @@ const Mypage = () => {
     { text: '댓그으을', userName: '오준상', star: 5 },
   ]);
   const [reviewDataArray, reviewDataArrayChange] = useState([
-    { name: '오준상', src: DEFAULT_USER_IMG, star: 1, id: 1, review: '1' },
+    { name: '오준상', src: DEFAULT_USER_IMG, star: 0, id: 1, review: '' },
   ]);
+  const [modalId, modalIdChange] = useState(-1);
   const [modalType, modalTypeChange] = useState('');
-  const modalDelete = () => {
+  const modalDelete = id => {
     modalTypeChange('');
+    if (typeof id === 'number') {
+      alarmDelete(id);
+    }
+  };
+  const alarmDelete = id => {
+    const buffer = noticeArray.filter(alarm => alarm.id !== id);
+    noticeArrayChange(buffer);
   };
   const setModalRequestModal = useCallback(() => {
     modalTypeChange('RequestApproveModal');
@@ -57,10 +65,8 @@ const Mypage = () => {
     modalTypeChange('ReportModal');
   });
   const setModalData = useCallback(() => {
-    console.log(modalType);
     if (modalType === 'RequestApproveModal') return requestDataArray;
     else if (modalType === 'ReviewModal') {
-      console.log(reviewDataArray[0]);
       return reviewDataArray;
     }
     return [];
@@ -70,6 +76,60 @@ const Mypage = () => {
     if (modalType === 'ReviewModal') return reviewDataArrayChange;
     return () => {};
   }, [requestDataArrayChange, reviewDataArrayChange, modalType]);
+  const getUserInfoAndSetState = useCallback(async () => {
+    try {
+      const { data } = await getUserInfo(id);
+      imgChange(data.imagepath);
+      userNameChange(data.name);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id]);
+  const recodeResponseToState = useCallback((response, index) => {
+    return response.map(recode => ({
+      id: index,
+      text: recode.title,
+    }));
+  }, []);
+  const getRecodeAndSetState = useCallback(async () => {
+    try {
+      const { data } = await getRecode(id);
+      const newState = recodeResponseToState(data);
+      storageArrayChange(newState);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id]);
+  const commentResponseToState = useCallback(response => {
+    return response.map(comment => ({
+      text: comment.score.score_content,
+      userName: comment.score.target_name,
+      star: comment.score.score,
+    }));
+  }, []);
+  const getCommentAndSetState = useCallback(async () => {
+    try {
+      const { data } = await getComment(id);
+      const newState = commentResponseToState(data);
+      commentsChange(newState);
+    } catch (error) {}
+  }, [id]);
+  const alarmResponseToState = useCallback((response, index) => {
+    return response.map(alarm => ({
+      text: alarm.content,
+      id: index,
+      type: alarm.type,
+    }));
+  }, []);
+  const getAlarmAndSetState = useCallback(async () => {
+    try {
+      const { data } = await getAlarm(id);
+      const newState = alarmResponseToState(data);
+      noticeArrayChange(newState);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id]);
   const renderNoticeComponent = useCallback(
     isMine => {
       if (isMine) {
@@ -78,12 +138,13 @@ const Mypage = () => {
             noticeContentArray={noticeArray}
             requestModalOn={setModalRequestModal}
             reviewModalOn={setModalReviewModal}
+            modalIdChange={modalIdChange}
           />
         );
       }
       return <AnotherComment comments={comments} />;
     },
-    [storageArray, setModalReviewModal],
+    [noticeArray, setModalReviewModal, setModalRequestModal],
   );
   const renderCommentComponent = useCallback(
     isMine => {
@@ -94,6 +155,12 @@ const Mypage = () => {
     },
     [comments],
   );
+  useEffect(() => {
+    getUserInfoAndSetState();
+    getAlarmAndSetState();
+    getCommentAndSetState();
+    getRecodeAndSetState();
+  }, []);
   return (
     <>
       <Modal
@@ -101,6 +168,8 @@ const Mypage = () => {
         onDelete={modalDelete}
         data={setModalData()}
         dataChange={setModalDataChangeFunction()}
+        modalId={modalId}
+        modalIdChange={modalIdChange}
       />
       <S.MypageDiv>
         <div>
