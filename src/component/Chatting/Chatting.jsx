7793402932e159556c27331b9150c9704ae9728d
ohjunsang <pandati@dsm.hs.kr>
2020-11-02 22:35:54 +0900

    [FIX] if chatting is null set header text to 'no chatting room'

--- src/component/Chatting/Chatting.jsx
+++ src/component/Chatting/Chatting.jsx
@@ -1,26 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import * as S from './style';
 import { Navigation } from './Navigation';
 import { ChattingContent } from './ChattingContent';
-import { useCallback } from 'react';
+import Socket, {
+  getChattingList,
+  getChattingLog,
+} from '../../lib/api/Chatting';
 
 const Chatting = () => {
-  const [chattingMember, chattingMemberChange] = useState([
-    { user: '누군가', img: '', id: 1, star: 3 },
-  ]);
-  const [chattingList, chattingListChange] = useState([{ id: 1, name: 'sth' }]);
-  const [chattingLogList, chattingLogListChange] = useState([
-    {
-      text: 'test',
-      isMine: true,
-      user: '누 군가',
-    },
-  ]);
+  const [chattingMember, chattingMemberChange] = useState([]);
+  const [chattingList, chattingListChange] = useState([]);
+  const [chattingLogList, chattingLogListChange] = useState([]);
   const [checkedChattingId, checkedChattingIdChange] = useState(1);
+  const [socket, socketChange] = useState(null);
   const getCheckedChatting = useCallback((checkedChattingId, chattingList) => {
-    return chattingList.filter(
+    const target = chattingList.filter(
       chatting => chatting.id === checkedChattingId,
     )[0];
+    if (typeof target === 'undefined') {
+      return false;
+    }
+    return target;
+  }, []);
+  const sendChatting = text => {
+    socket.sendChatting(text);
+  };
+  const getChattingListResponseToState = useCallback(response => {
+    return response.map(chattingListElement => ({
+      id: chattingListElement.chat_id,
+      name: chattingListElement.chat_name,
+    }));
+  }, []);
+  const getChattingListAndSetState = useCallback(async () => {
+    const { data } = await getChattingList();
+    const newState = getChattingListResponseToState(data);
+    chattingListChange(newState);
+  }, []);
+  const getChattingLogResponseToState = useCallback(response => {
+    return response.map(chattingLogElement => ({
+      text: chattingLogElement.message,
+      user: chattingLogElement.sender,
+    }));
+  }, []);
+  const getChattingLogAndSetState = useCallback(async roomId => {
+    const { data } = await getChattingLog(roomId);
+    const newState = getChattingLogResponseToState(data);
+    chattingLogListChange(newState);
+  }, []);
+  useEffect(() => {
+    getChattingListAndSetState();
+    getChattingLogAndSetState();
+    const socket = new Socket();
+    socketChange(socket);
   }, []);
   return (
     <S.Chatting>
@@ -33,6 +64,7 @@ const Chatting = () => {
         target={getCheckedChatting(checkedChattingId, chattingList)}
         chattingLogList={chattingLogList}
         chattingMember={chattingMember}
+        sendChatting={sendChatting}
       />
     </S.Chatting>
   );
