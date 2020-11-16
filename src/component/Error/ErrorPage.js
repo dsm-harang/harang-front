c59f846bb4ca769404d34e21cd5c9449bbd5b765
ohjunsang <pandati@dsm.hs.kr>
2020-11-16 09:33:17 +0900

    [CREATE] @/component/Error errorpage

---
+++ src/component/Error/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+const ErrorPage = () => {
+  return (
+    <S.ErrorPage>
+      <S.ErrorText>404 Not Found</S.ErrorText>
+    </S.ErrorPage>
+  );
+};
+
+export default ErrorPage;
