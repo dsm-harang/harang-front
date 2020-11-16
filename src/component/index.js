2657ea9608d03e08dba20624452e9df6e1d578c7
ohjunsang <pandati@dsm.hs.kr>
2020-11-16 14:04:34 +0900

    [FIX] @/component export 방법 수정

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,17 @@
-import { Mypage } from './MyPage';
-import { Chatting } from './Chatting';
-import { Map } from './Map';
-export { Mypage, Chatting, Map };
+import Mypage from './MyPage';
+import Chatting from './Chatting';
+import Map from './Map';
+import AdminPost from './AdminReport';
+import AdminUserSearch from './AdminUserSearch';
+import AdminUserDetail from './AdminUserDetail';
+import ErrorPage from './Error';
+
+export {
+  Mypage,
+  Chatting,
+  Map,
+  AdminPost,
+  AdminUserSearch,
+  AdminUserDetail,
+  ErrorPage,
+};
