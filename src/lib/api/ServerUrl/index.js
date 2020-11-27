911f92f18eedfb02356237258193488e710218e6
ohjunsang <pandati@dsm.hs.kr>
2020-11-27 14:06:43 +0900

    [FIX] fix server url

--- src/lib/api/ServerUrl/index.js
+++ src/lib/api/ServerUrl/index.js
@@ -6,10 +6,9 @@ export const RECODE_URL = '/mypage/post';
 export const USER_REPORT_URL = '/user/report';
 export const APPROVE_URL = '/post/accept';
 export const GET_COMMENT_REQUIRE_LIST_URL = 'mypage/scoreList';
-export const GET_APPROVE_LIST_URL = '/post/accept/list/';
+export const GET_APPROVE_LIST_URL = '/post/accept/list';
 
 export const CHATTING_URL = '/chat';
-export const ROOMS_URL = '/rooms';
 export const ROOM_URL = '/room';
 
 export const ADMIN_URL = '/admin';
@@ -21,3 +20,4 @@ export const ADMIN_REPORT_USER_URL = '/admin/report/user';
 export const ADMIN_LOGIN_URL = '/auth/admin';
 
 export const USER_URL = '/user';
+export const CHATTING_BASE_URL = '';
